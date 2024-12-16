16e8cbf5435f467ab7357d15417a757d3a97c3f2
LOVELY <kotagindeman@gmail.com>
2024-12-16 17:19:16 +0300

    feat: add Class Rabbits

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
+import random
+
 class Tiger:
     def __init__(self):
         self.state = 'hunt prey'
         self.lucky_attack = 0.5
         self.cord_x = 0
         self.cord_y = 0
+
+    def random_stroll(self):
+        self.cord_x += random.choice([-1, 0, 1])
+        self.cord_y += random.choice([-1, 0, 1])
+        self.cord_x = max(0, min(self.cord_x, 4))
+        self.cord_y = max(0, min(self.cord_y, 4))
+
+    def ubdate_state(self):
+        if self.state == 'hunt prey':
+            print('tiger is hunting')
+            self.random_stroll()
+
+
+
+class Rabbits:
+    def __init__(self, x, y):
+        self.is_find = False
+        self.cord_x = x
+        self.cord_y = y
+
+
+    def to_find(self):
+        self.is_find = True
